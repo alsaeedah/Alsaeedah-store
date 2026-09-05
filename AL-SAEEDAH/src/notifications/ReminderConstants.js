dc6db1a9157d6920d389f023c7a7104b12fc6c14
alsaeedah <alsaeedah8@gmail.com>
2026-09-05 13:33:50 +0300

    test success

--- AL-SAEEDAH/src/notifications/ReminderConstants.js
+++ AL-SAEEDAH/src/notifications/ReminderConstants.js
@@ -8,7 +8,7 @@
 const ONE_DAY_MS = 24 * 60 * 60 * 1000;
 
 export const REMINDERS = {
-  THREE_DAYS: { delay: 60 * 1000 }, // تم التعديل مؤقتاً إلى دقيقة واحدة للاختبار
+  THREE_DAYS: { delay: 3 * ONE_DAY_MS },
   SEVEN_DAYS: { delay: 7 * ONE_DAY_MS },
   FOURTEEN_DAYS: { delay: 14 * ONE_DAY_MS }
 };
